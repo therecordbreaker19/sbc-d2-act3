c7d668ad2ade7db02ebf4b93706030082b7a367f
Jared Burdeos <132859086+therecordbreaker19@users.noreply.github.com>
2024-06-19 04:41:37 -0700

    Add files via upload

---
+++ sbc-d2-act3.py
@@ -0,0 +1,25 @@
+from random import randint
+
+
+num1 = int(input("ENTER FIRST NUMBER: "))
+num2 = int(input("ENTER SECOND NUMBER: "))
+num3 = int(input("ENTER THIRD NUMBER: "))
+
+
+gen1 = randint(1, 5)
+gen2 = randint(1, 5)
+gen3 = randint(1, 5)
+
+
+print(f"USER INPUT: {num1} {num2} {num3}")
+print(f"GENERATED NUMBERS: {gen1} {gen2} {gen3}")
+
+
+if [num1, num2, num3] == [gen1, gen2, gen3]:
+    print("You Win!")
+else:
+
+    if sorted([num1, num2, num3]) == sorted([gen1, gen2, gen3]):
+        print("You partially win!")
+    else:
+        print("You lose!")
